bb62a1e61f943c38b215ce9968ba41dc2f8fc587
paulocesarvasco <paulocvasco.93@gmail.com>
2025-02-18 15:23:20 -0300

    Leet code exercise 94

---
+++ tree/leet/go.mod
@@ -0,0 +1,3 @@
+module Data-structures-Golang/tree/leet
+
+go 1.23.6
---
+++ tree/leet/inorder/main.go
@@ -0,0 +1,22 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func inorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return []int{}
+	}
+	var res []int
+	if root.Left != nil {
+		res = append(res, inorderTraversal(root.Left)...)
+	}
+	res = append(res, root.Val)
+	if root.Right != nil {
+		res = append(res, inorderTraversal(root.Right)...)
+	}
+	return res
+}
